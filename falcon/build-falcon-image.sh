5d80cd58ccb920a09e2bac27ecd2e7a0262b3e91
allenz1120 <55994268+allenz1120@users.noreply.github.com>
2021-10-05 14:56:00 -0400

    Delete falcon directory

--- falcon/build-falcon-image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Adapted from https://embexus.com/2017/05/07/fast-boot-linux-with-u-boot-falcon-mode/
-dd if=/dev/zero of=sdcard.img bs=1M count=64
-dd if=am335x-boneblack.dtb of=sdcard.img bs=1 seek=65536
-dd if=MLO of=sdcard.img bs=1 seek=262144
-dd if=u-boot.img of=sdcard.img bs=1 seek=393216
-dd if=uImage of=sdcard.img bs=1 seek=1179648
