7263c02e4d60486b4513485a2e4c946c4ee4f04c
Allen Zou <allenzou@bu.edu>
2021-10-14 15:11:33 -0400

    changed arm linux patch and added setup project environment script

---
+++ scripts/setupenvironment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+####################################################
+## Environment Setup script for MicroFaaS Project ##
+## Boston University PEACLAB 2021                 ##
+####################################################
+
+sudo apt install -y libssl-dev gcc-arm-linux-gnueabihf bison flex build-essential gcc-arm-linux-gnueabihf u-boot-tools sed make binutils gcc g++ bash patch gzip bzip2 perl tar cpio python unzip rsync wget libncurses-dev
+
+#chmod bobthebuilder.sh
+chmod +x bobthebuilder.sh
+
+mkdir ../../projectName
+cd ../../projectName
+mv ../MicroFaaS .
+
+#Clone linux 5.11.22
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+git remote add stable git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+git fetch stable
+git checkout stable/linux-5.11.y
+make CROSS_COMPILE=arm-linux-gnueabihf- ARCH=arm omap2plus_defconfig
+
+#Clone linux-initramfs with submodules
+git clone --recurse-submodules -j8 git@github.com:peaclab/linux-initramfs.git
+
